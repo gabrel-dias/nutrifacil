3131c654a5bfa25b7200019240f87e57d9f7169f
Gabriel Dias <gabrielmdais557@gmail.com>
2024-11-19 01:14:23 -0300

    cadastro atuaizado com novos dados de usuário

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -6,20 +6,20 @@ const banco_supabase = supabase.createClient(PROJECT_URL, API_KEY);
 // Captura os campos e o formulário, e adiciona os eventos de clique e input
 document.addEventListener('DOMContentLoaded', () => {
   const formCadastro = document.getElementById('formCadastro');
-  const nome_usuario_input = document.getElementById('nome_usuario');
-  const nome_verificacao = document.getElementById('nome_verificacao');
+  const email_usuario_input = document.getElementById('EMAIL');
+  const nome_verificacao = document.getElementById('email_verificacao');
 
   // Verifica o nome conforme o usuário digita
-  nome_usuario_input.addEventListener('input', async () => {
-    const nome_usuario = nome_usuario_input.value;
+  email_usuario_input.addEventListener('input', async () => {
+    const email_usuario = email_usuario_input.value;
 
     // Verifica se o nome está no banco de dados
-    const existe = await verificarNomeUsuario(nome_usuario);
+    const existe = await verificarEmailUsuario(email_usuario);
     if (existe) {
-      nome_verificacao.textContent = `O nome ${nome_usuario} já está presente no banco de dados.`;
+      nome_verificacao.textContent = `O e-mail ${email_usuario} já está presente no banco de dados.`;
       nome_verificacao.style.color = 'red';
     } else {
-      nome_verificacao.textContent = `O nome ${nome_usuario} está disponível.`;
+      nome_verificacao.textContent = `O e-mail ${email_usuario} está disponível.`;
       nome_verificacao.style.color = 'green';
     }
   });
@@ -28,21 +28,33 @@ document.addEventListener('DOMContentLoaded', () => {
   formCadastro.addEventListener('submit', async (event) => {
     event.preventDefault(); // Previne o envio padrão do formulário
 
-    const nome_completo = document.getElementById('nome_completo').value;
-    const nome_usuario = document.getElementById('nome_usuario').value;
-    const senha_usuario = document.getElementById('senha_usuario').value;
-    const usuario_admin = document.getElementById('usuario_admin').value;
+    // informações pessoais do usuario e seu tipo
+    const nome = document.getElementById('NOME').value;
+    const cpf = document.getElementById('CPF').value;
+    const email = document.getElementById('EMAIL').value;
+    const senha = document.getElementById('SENHA').value;
+    const telefone = document.getElementById('TELEFONE').value;
+    const tipo_usuario = document.getElementById('TIPOUSUARIO').value;
 
-    await inserirUsuario(nome_completo, nome_usuario, senha_usuario, usuario_admin);
+    //endereço do usuário
+    const cep = document.getElementById('CEP').value;
+    const logradouro = document.getElementById('LOGRADOURO').value;
+    const numero = document.getElementById('NUMERO').value;
+    const bairro = document.getElementById('BAIRRO').value;
+    const cidade = document.getElementById('CIDADE').value;
+    const uf = document.getElementById('UF').value;
+
+
+    await inserirUsuario(nome, cpf, email, senha, telefone, tipo_usuario, cep, logradouro, numero, bairro, cidade, uf);
   });
 });
 
 // Função para inserir dados
-async function inserirUsuario(nome_completo, nome_usuario, senha_usuario, usuario_admin) {
+async function inserirUsuario(nome, cpf, email, senha, telefone, tipo_usuario, cep, logradouro, numero, bairro, cidade, uf) {
   try {
     const { error } = await banco_supabase
-      .from('usuarios')
-      .insert({ nome_completo: nome_completo, nome_usuario: nome_usuario, senha_usuario: senha_usuario, admin: usuario_admin });
+      .from('TBUsuarios')
+      .insert({ NOME: nome, CPF: cpf, EMAIL: email, SENHA: senha, TELEFONE: telefone, TIPOUSUARIO: tipo_usuario, CEP: cep, LOGRADOURO: logradouro, NUMERO: numero, BAIRRO: bairro, CIDADE: cidade, UF: uf });
     if (error) {
       console.error('Erro ao inserir dados:', error.message);
     } else {
@@ -53,33 +65,13 @@ async function inserirUsuario(nome_completo, nome_usuario, senha_usuario, usuari
   }
 }
 
-// Função para verificar se o ID do usuário está no banco de dados
-async function verificarIdUsuario(id_usuario) {
+
+async function verificarEmailUsuario(EMAIL) {
   try {
     const { data, error } = await banco_supabase
-      .from('usuarios')
-      .select('id')
-      .eq('id', id_usuario);
-
-    if (error) {
-      console.error('Erro ao verificar ID:', error.message);
-      return false;
-    }
-
-    return data.length > 0;
-  } catch (err) {
-    console.error('Erro inesperado:', err);
-    return false;
-  }
-}
-
-// Função para verificar se o nome do usuário está no banco de dados
-async function verificarNomeUsuario(nome_usuario) {
-  try {
-    const { data, error } = await banco_supabase
-      .from('usuarios')
-      .select('nome_usuario')
-      .eq('nome_usuario', nome_usuario);
+      .from('TBUsuarios')
+      .select('EMAIL')
+      .eq('EMAIL', EMAIL);
 
     if (error) {
       console.error('Erro ao verificar nome:', error.message);
